6e7454b567d2f9cf7acb53dffdf25b36f3e76002
Sean Henderson <mail@sean.sh>
2013-12-24 01:16:59 +0545

    Add random url generation.

--- rytc.rb
+++ rytc.rb
@@ -1,15 +1,23 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'securerandom'
 
-# test
+# test random yt urls
+gen = SecureRandom.urlsafe_base64(8)
+url = "https://gdata.youtube.com/feeds/api/videos/" + gen + "/comments"
+puts url
+
+# test comment getting
 page = Nokogiri::XML(open("https://gdata.youtube.com/feeds/api/videos/a79t3s5yig8/comments"))
 comment = page.css("content")[0].text
 puts comment
 
-# generate a random string matching youtube url format
-#
-# check if the string is a working youtube video
+# test check if the string is a working youtube video
+
+
+
+
 # 
 # hit gdata.youtube.com with request for xml
 #
