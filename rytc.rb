d05593a99a732369deb07dab52e7365d1be717a7
Sean Henderson <mail@sean.sh>
2013-12-24 16:28:46 +0545

    Comment out successful tests, continue valid checker.

--- rytc.rb
+++ rytc.rb
@@ -3,24 +3,28 @@ require 'nokogiri'
 require 'open-uri'
 require 'securerandom'
 
-# test random yt urls
-gen = SecureRandom.urlsafe_base64(8)
-url = "https://gdata.youtube.com/feeds/api/videos/" + gen + "/comments"
-puts url
+# SUCCESS random yt urls
+#gen = SecureRandom.urlsafe_base64(8)
+#randomurl = "https://gdata.youtube.com/feeds/api/videos/" + gen + "/comments"
+#puts randomurl
 
-# test comment getting
-page = Nokogiri::XML(open("https://gdata.youtube.com/feeds/api/videos/a79t3s5yig8/comments"))
-comment = page.css("content")[0].text
-puts comment
+# SUCCESS comment getting
+#page = Nokogiri::XML(open("https://gdata.youtube.com/feeds/api/videos/a79t3s5yig8/comments"))
+#comment = page.css("content")[0].text
+#puts comment
 
-# test check if the string is a working youtube video
-
-
-begin # fail test
-  url = Nokogiri::XML(open("https://gdata.youtube.com/feeds/api/videos/T-4FvTDo5pg/comments"))
+# check if the string is a working youtube video
+begin # if valid url is generated will return raw XML
+  gen = SecureRandom.urlsafe_base64(8)
+  randomurl = "https://gdata.youtube.com/feeds/api/videos/#{gen}/comments"  # api req for with random video id
+  #randomurl = "https://gdata.youtube.com/feeds/api/videos/HjKcCGaCiu0/comments"
+  url = Nokogiri::XML(open(randomurl))
+  puts url
 rescue OpenURI::HTTPError
-  puts "#{url} is not a valid youtube video."
+  puts "#{randomurl} is not a valid youtube."
+  retry
 end
+
 
 # see http://blog.jerodsanto.net/2008/08/ruby-to-the-rescue/
 
