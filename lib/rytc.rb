4547294308db4376e8681e79aa61149aa03baea9
Sean Henderson <mail@sean.sh>
2013-12-25 13:42:44 +0545

    Set version to 0.0.0; define class Rytc with go method.

--- lib/rytc.rb
+++ lib/rytc.rb
@@ -2,31 +2,33 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+class Rytc
+  def self.go
+    begin
+      # get randomyoutubecomment.com
+      page = Nokogiri::HTML(open("http://www.randomyoutubecomment.com/"))
 
-begin
-  # get randomyoutubecomment.com
-  page = Nokogiri::HTML(open("http://www.randomyoutubecomment.com/"))
+      # get the comment
+      comment = page.css("div a p")[0].text
+      # @ filter out strange characters
 
-  # get the comment
-  comment = page.css("div a p")[0].text
-  # @ filter out strange characters
+      # get the author
+      author = page.css("div a + p")[0].text
 
-  # get the author
-  author = page.css("div a + p")[0].text
+      # get the video url
+      vidurl = page.css("div p:nth-child(4) a")[0]["href"]
+    
+      # output the comment
+      puts comment
+      puts author
+      puts vidurl
 
-  # get the video url
-  vidurl = page.css("div p:nth-child(4) a")[0]["href"]
- 
-  # output the comment
-  puts comment
-  puts author
-  puts vidurl
+    rescue OpenURI::HTTPError
 
-rescue OpenURI::HTTPError
+      puts "Couldn't get the page - OpenURL::HTTPError"
+      puts "Trying again..."
+      retry
 
-  puts "Couldn't get the page - OpenURL::HTTPError"
-  puts "Trying again..."
-  retry
-
+    end
+  end
 end
-
--- rytc.gemspec
+++ rytc.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'rytc'
-  s.version     = '1.0.1'
+  s.version     = '0.0.0'
   s.date        = '2013-12-25'
   s.summary     = "Random Youtube Comment"
   s.description = "Generate a comment from a random youtube video anywhere in your application."
