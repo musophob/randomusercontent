2d41b5ddfa4cbab6b8703ec2a9bd2d0fed99a5fe
Sean Henderson <mail@sean.sh>
2013-12-25 13:17:58 +0545

    Revamp to use randomyoutubecomment.com.
    Leverage http://randomyoutubecomment.com/ for content generation.

--- rytc.rb
+++ rytc.rb
@@ -1,36 +1,32 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-require 'securerandom'
 
-# SUCCESS random yt urls
-#gen = SecureRandom.urlsafe_base64(8)
-#randomurl = "https://gdata.youtube.com/feeds/api/videos/" + gen + "/comments"
-#puts randomurl
 
-# SUCCESS comment getting
-#page = Nokogiri::XML(open("https://gdata.youtube.com/feeds/api/videos/a79t3s5yig8/comments"))
-#comment = page.css("content")[0].text
-#puts comment
+begin
+  # get randomyoutubecomment.com
+  page = Nokogiri::HTML(open("http://www.randomyoutubecomment.com/"))
 
-# check if the string is a working youtube video
-begin # if valid url is generated will return raw XML
-  gen = SecureRandom.urlsafe_base64(8)
-  randomurl = "https://gdata.youtube.com/feeds/api/videos/#{gen}/comments"  # api req for with random video id
-  #randomurl = "https://gdata.youtube.com/feeds/api/videos/HjKcCGaCiu0/comments"
-  url = Nokogiri::XML(open(randomurl))
-  puts url
+  # get the comment
+  comment = page.css("div a p")[0].text
+  # @ filter out strange characters
+
+  # get the author
+  author = page.css("div a + p")[0].text
+
+  # get the video url
+  vidurl = page.css("div p:nth-child(4) a")[0]["href"]
+ 
+  # output the comment
+  puts comment
+  puts author
+  puts vidurl
+
 rescue OpenURI::HTTPError
-  puts "#{randomurl} is not a valid youtube."
+
+  puts "Couldn't get the page - OpenURL::HTTPError"
+  puts "Trying again..."
   retry
+
 end
 
-
-# see http://blog.jerodsanto.net/2008/08/ruby-to-the-rescue/
-
-# 
-# hit gdata.youtube.com with request for xml
-#
-# generate a random number to select comment and parse out comment from xml
-#
-# return the the text
